e6d543db04d206cd1f0ba9920d37f99fa1dc8c71
Alex Baranosky <alexander.baranosky@gmail.com>
2012-02-12 02:08:57 -0500

    expanded tests to add TODO items for different types

--- test/shrink/test/t_shrink.clj
+++ test/shrink/test/t_shrink.clj
@@ -20,6 +20,25 @@
     [0]     [[]]
     [1 2]   [[1] [2] [0 2] [1 0] [1 1] [1 -1]]
 
+    ;; chars
+    \c []      
+    
+    ;; strings
+    [\a]        [[]]
+    [\a \b]     [[\a] [\b]]
+    [\a \b \c]  [[\a] [\b \c]]
   
     ;; can't shrink things it doesn't understand
-    (Foo.) []  )
+    (Foo.) []
+    \z     []
+    
+    ;; TODO
+    :abc              []
+    'abc              []
+     #{1 2 3}         []
+    {:a 1 :b 2}       []
+    (sorted-set #{})  []
+    (sorted-map :a 1) []
+    '(1 2 3)          []
+    ;; 4.5            []
+  )
