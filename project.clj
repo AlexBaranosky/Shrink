70fef035a0a8b526981c4eae6702021e93e60efc
Alex Baranosky <alexander.baranosky@gmail.com>
2012-02-12 12:12:05 -0500

    cleanup; removed core.match

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject shrink "0.1.0"
   :description "Functions for shrinking failure cases in generative testing"
-  :dependencies [[org.clojure/clojure "1.4.0-beta1"]
-                 [org.clojure/core.match "0.2.0-alpha9"]]
+  :dependencies [[org.clojure/clojure "1.4.0-beta1"]]
   :dev-dependencies [[midje "1.3.2-SNAPSHOT"]])
--- src/shrink/shrink.clj
+++ src/shrink/shrink.clj
@@ -1,15 +1,4 @@
-(ns shrink.shrink
-  (:use [clojure.core.match :only [match]]))
-
-(defmacro defmatch [name args & cases]
-  `(defn ~name ~args
-     (match ~args
-       ~@cases)))
-
-(defmacro defmethod-match [name dispatch-val args & cases]
-  `(defmethod ~name ~dispatch-val ~args
-     (match ~args
-       ~@cases)))
+(ns shrink.shrink)
 
 (defn- long-div [& ns]
   (long (apply / ns)))
@@ -23,53 +12,54 @@
                      (string? x) :string
                      )))
 
-(defn- remove-chunks [xs]
-  (cond
-    (empty? xs)       []
-    (= 1 (count xs))  [[]]
-    :else             (let [length (count xs)
-                            n1 (long-div length 2)
-                            n2 (- length n1)
-                            [xs1 xs2] [(take n1 xs) (drop n1 xs)]
-                            xs3 (for [ys1 (remove-chunks xs1)
-                                      :when (seq ys1)]
-                                   (concat ys1 xs2))
-                            xs4 (for [ys2 (remove-chunks xs2)
-                                      :when (seq ys2)]
-                                   (concat xs1 ys2))]
-                        (list* xs1 xs2 (interleave xs3 xs4)))))
+(letfn [(remove-chunks [xs]
+          (cond
+            (empty? xs)       []
+            (= 1 (count xs))  [[]]
+            :else             (let [length (count xs)
+                                    n1 (long-div length 2)
+                                    n2 (- length n1)
+                                    [xs1 xs2] [(take n1 xs) (drop n1 xs)]
+                                    xs3 (for [ys1 (remove-chunks xs1)
+                                              :when (seq ys1)]
+                                           (concat ys1 xs2))
+                                    xs4 (for [ys2 (remove-chunks xs2)
+                                              :when (seq ys2)]
+                                           (concat xs1 ys2))]
+                                (list* xs1 xs2 (interleave xs3 xs4)))))
 
-(defn- shrink-one [zs]
-  (cond
-    (empty? zs) []
-    :else       (let [[x & xs] zs
-                      a (for [y (shrink x)]
-                          (cons y xs))
-                      b (for [ys (shrink-one xs)]
-                          (cons x ys))]
-                  (concat a b))))
+      (shrink-one [zs]
+        (cond
+          (empty? zs) []
+          :else       (let [[x & xs] zs
+                            a (for [y (shrink x)]
+                                (cons y xs))
+                            b (for [ys (shrink-one xs)]
+                                (cons x ys))]
+                        (concat a b))))]
 
-(defmethod shrink :vector [xs]
-  (concat (remove-chunks xs) (shrink-one xs)))
-
-(defmethod shrink :list [xs]
-  (concat (remove-chunks xs) (shrink-one xs)))
-
-(defmethod shrink :seq [xs]
-  (concat (remove-chunks xs) (shrink-one xs)))
-
-(defn- halfs [n] 
-  (if (> 1 n) 
-    []
-    (cons n (halfs (long-div n 2)))))
-
-(defmethod-match shrink :int [x]
-  [0]  []
-  [_]  (let [ns (map #(- x %) (halfs (long-div x 2)))
-             negated-ns (map (partial * -1) ns)]
-         (cons 0 (interleave ns negated-ns))))
+  (defmethod shrink :vector [xs]
+    (concat (remove-chunks xs) (shrink-one xs)))
+  
+  (defmethod shrink :list [xs]
+    (concat (remove-chunks xs) (shrink-one xs)))
+  
+  (defmethod shrink :seq [xs]
+    (concat (remove-chunks xs) (shrink-one xs))))
 
 (defmethod shrink :string [s]
   (map (partial apply str) (shrink (seq s))))
+
+(letfn [(halfs [n] 
+          (if (> 1 n) 
+            []
+            (cons n (halfs (long-div n 2)))))]
+
+  (defmethod shrink :int [x]
+    (if (= x 0)
+      []
+      (let [ns (map #(- x %) (halfs (long-div x 2)))
+            negated-ns (map (partial * -1) ns)]
+        (cons 0 (interleave ns negated-ns))))))
 
 (defmethod shrink :default [_] [])
